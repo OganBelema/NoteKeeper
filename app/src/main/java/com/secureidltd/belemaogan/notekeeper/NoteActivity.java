2d490a6ba0a8264f2b1610a7a16f3ccffad969b8
Ogan Belema <Oganbelema@gmail.com>
2018-11-05 20:29:51 +0100

    Activities reside in processes A task is a group of activities required to perform an operation Implicit intents are usually instantiated with an action parameter When android system installs your app, your manifest file is stored so the system knows the intents your app can handle. Send email intent in code is an example of implicit intent You can also start activities for result, you specify a request code and check the response code against the RESULT_OK constant. In the onActivityResult is an intent that contains the result from the activity that handled your intent. The onPause method is called when an activity is closing, you can use that to save data

--- app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteActivity.java
+++ app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteActivity.java
@@ -2,11 +2,8 @@ package com.secureidltd.belemaogan.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -15,11 +12,21 @@ import android.widget.Spinner;
 
 import java.util.List;
 
+/**
+ * Created by Ogan Belema
+ **/
+
 public class NoteActivity extends AppCompatActivity {
 
-    public static final String NOTE_INFO = "com.secureidltd.belemaogan.notekeeper.NOTE_INFO";
+    public static final String NOTE_POSITION = "com.secureidltd.belemaogan.notekeeper.NOTE_POSITION";
+    public static final int DEFAULT_POSITION_VALUE = -1;
     private NoteInfo mNoteInfo;
     private Boolean mIsNewNote = false;
+    private Spinner mSpinnerCourses;
+    private EditText mTextNoteTitle;
+    private EditText mTextNoteText;
+    private Boolean mIsCanceling = false;
+    private int mNewNotePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +35,49 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+        mSpinnerCourses = findViewById(R.id.spinner_courses);
 
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         ArrayAdapter<CourseInfo> adapterCourses =
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
 
-        spinnerCourses.setAdapter(adapterCourses);
+        mSpinnerCourses.setAdapter(adapterCourses);
 
 
         readDisplayStateValues();
 
-        EditText textNoteTitle = findViewById(R.id.text_note_title);
-        EditText textNoteText = findViewById(R.id.text_note_text);
+        mTextNoteTitle = findViewById(R.id.text_note_title);
+        mTextNoteText = findViewById(R.id.text_note_text);
 
-        if (!mIsNewNote){
-            displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        if (mIsNewNote){
+            createNewNote();
+        } else {
+            displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
         }
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        mNewNotePosition = dm.createNewNote();
+        mNoteInfo = dm.getNotes().get(mNewNotePosition);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mIsCanceling){
+            if (mIsNewNote){
+                DataManager.getInstance().removeNote(mNewNotePosition);
+            }
+        } else {
+            saveNote();
+        }
+    }
+
+    private void saveNote() {
+        mNoteInfo.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());
+        mNoteInfo.setTitle(mTextNoteTitle.getText().toString());
+        mNoteInfo.setText(mTextNoteText.getText().toString());
     }
 
     private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
@@ -64,8 +97,9 @@ public class NoteActivity extends AppCompatActivity {
     private void readDisplayStateValues() {
         Intent intent = getIntent();
 
-        if (intent != null && intent.hasExtra(NOTE_INFO)){
-            mNoteInfo = intent.getParcelableExtra(NOTE_INFO);
+        if (intent != null && intent.hasExtra(NOTE_POSITION)){
+            int position = intent.getIntExtra(NOTE_POSITION, DEFAULT_POSITION_VALUE);
+            mNoteInfo = DataManager.getInstance().getNotes().get(position);
         } else {
             mIsNewNote = true;
         }
@@ -86,10 +120,31 @@ public class NoteActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_send_mail) {
+            sendMail();
+            return true;
+        } else if (id == R.id.action_cancel){
+            mIsCanceling = true;
+            finish();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendMail() {
+
+        CourseInfo courseInfo = (CourseInfo) mSpinnerCourses.getSelectedItem();
+
+        String subject = mTextNoteTitle.getText().toString();
+
+        String text = "Learning \""+ courseInfo.getTitle() +"\" on Pluralsight\n"
+                + mTextNoteText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
     }
 }
