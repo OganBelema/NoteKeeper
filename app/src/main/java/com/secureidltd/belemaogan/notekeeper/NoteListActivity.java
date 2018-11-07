d72878a9d6a03cd36668e3bf82bd1249fe9101a2
Ogan Belema <Oganbelema@gmail.com>
2018-11-07 17:32:53 +0100

    Recyclerview and CardView Replaced listView with Recyclerview Created layout for the recyclerview items and used a card to provide a 3d feel to the list. Key requirements for a recyclerview are the item view layout, layout manager and an adapter.

--- app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,7 +20,7 @@ import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
-    private ArrayAdapter<NoteInfo> mAdapterNotes;
+    //private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +41,7 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     private void initializeDisplayContent() {
-        final ListView listNotes = findViewById(R.id.list_notes);
+        /*final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
 
@@ -55,12 +57,15 @@ public class NoteListActivity extends AppCompatActivity {
                 intent.putExtra(NoteActivity.NOTE_POSITION, position);
                 startActivity(intent);
             }
-        });
+        });*/
+        final RecyclerView recyclerViewNotes = findViewById(R.id.list_notes);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewNotes.setLayoutManager(layoutManager);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterNotes.notifyDataSetChanged();
+        //mAdapterNotes.notifyDataSetChanged();
     }
 }
