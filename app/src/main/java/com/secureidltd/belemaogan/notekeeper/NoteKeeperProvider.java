3a9b1d53dde99437e1807b56e6b7be2c6650e3e4
Ogan Belema <Oganbelema@gmail.com>
2018-12-05 15:45:15 +0100

    Added the join table statement and the qualifiers for the columns to the content provider. Content Providers - They expose tables - Normally expose multiple tables - Often correspond to database tables - May also provide an abstraction - Each table has an individual URI
    -> Content providers usually have contract class
    - Presents public appearance
    - Includes classes for contained tables
    
    -> Tables classes
    - Nested classes within URI
    - Contain table URI
    - Name of available tables
    
    -> Column name constants
    - Often defined in interfaces
    - Interfaces nested in contract class
    - Provides central management
    - Tables implement appropriate interfaces
    
    -> Table URIs
    - Starts with content scheme
    - Includes provider authority
    - Table is added as a path
    
    -> Handling table URIs
    - Use UriMatcher class
    - Translates URI to an integer constant

--- app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteKeeperProvider.java
+++ app/src/main/java/com/secureidltd/belemaogan/notekeeper/NoteKeeperProvider.java
@@ -2,15 +2,32 @@ package com.secureidltd.belemaogan.notekeeper;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 import com.secureidltd.belemaogan.notekeeper.NoteKeeperDatabaseContract.CourseInfoEntry;
+import com.secureidltd.belemaogan.notekeeper.NoteKeeperDatabaseContract.NoteInfoEntry;
+import com.secureidltd.belemaogan.notekeeper.NoteKeeperProviderContract.Courses;
+import com.secureidltd.belemaogan.notekeeper.NoteKeeperProviderContract.CoursesIdColumns;
+import com.secureidltd.belemaogan.notekeeper.NoteKeeperProviderContract.Notes;
 
 public class NoteKeeperProvider extends ContentProvider {
 
     private NoteKeeperOpenHelper mNoteKeeperOpenHelper;
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private static final int COURSES = 0;
+    private static final int NOTES = 1;
+    private static final int NOTES_EXPANDED = 2;
+
+    static {
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Courses.PATH, COURSES);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH, NOTES);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH_EXPANDED, NOTES_EXPANDED);
+    }
 
     public NoteKeeperProvider() {
     }
@@ -46,9 +63,40 @@ public class NoteKeeperProvider extends ContentProvider {
                         String[] selectionArgs, String sortOrder) {
         Cursor cursor = null;
         SQLiteDatabase sqLiteDatabase = mNoteKeeperOpenHelper.getReadableDatabase();
-        cursor = sqLiteDatabase.query(CourseInfoEntry.TABLE_NAME, projection, selection,
-                selectionArgs, null, null, sortOrder);
+        int match = sUriMatcher.match(uri);
+        switch (match){
+            case COURSES:
+                cursor = sqLiteDatabase.query(CourseInfoEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            case NOTES:
+                cursor = sqLiteDatabase.query(NoteInfoEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            case NOTES_EXPANDED:
+                cursor = notesExpandedQuery(sqLiteDatabase, projection, selection, selectionArgs, sortOrder);
+                break;
+        }
+
         return cursor;
+    }
+
+    private Cursor notesExpandedQuery(SQLiteDatabase sqLiteDatabase, String[] projection, String selection,
+                                      String[] selectionArgs, String sortOrder) {
+        String[] columns = new String[projection.length];
+        for (int index = 0; index < projection.length; index++){
+            //if column[index] = projection[index].equals(BaseColumns._ID) do...else
+            columns[index] = projection[index].equals(BaseColumns._ID) ||
+                    projection[index].equals(CoursesIdColumns.COLUMN_COURSE_ID) ?
+                    NoteInfoEntry.getQName(projection[index]) : projection[index];
+        }
+        String tablesWithJoin = NoteInfoEntry.TABLE_NAME + " JOIN " + CourseInfoEntry.TABLE_NAME
+                + " ON " + NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID) + " = "
+                + CourseInfoEntry.getQName(CourseInfoEntry.COLUMN_COURSE_ID);
+        return sqLiteDatabase.query(tablesWithJoin, columns, selection, selectionArgs,
+                null, null, sortOrder);
     }
 
     @Override
